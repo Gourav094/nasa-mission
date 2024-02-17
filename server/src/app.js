5b8fa0da73a991540f5e89392839e18f8873b41e
gourav garg <garggourav012@gmail.com>
2024-02-17 17:25:28 +0530

    deploy frontend & backend on same endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const planetRouter = require('./routes/planet/planet.router')
+const path = require('path')
 const express = require('express')
 const app  = express()
 const cors = require('cors')
@@ -7,6 +8,10 @@ app.use(cors({
     origin:'http://localhost:3000'
 }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetRouter)
 
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 module.exports = app
